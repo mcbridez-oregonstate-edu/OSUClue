40d06f5f4dc668940982ed148dabdecf5891c5b8
mcbridz@gmail.com <mcbridz@gmail.com>
2020-04-14 17:21:51 -0700

    boardTile header file and main tester

---
+++ loadClue.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+using namespace std;
+
+//this is the demo main for displaying the clue board. as a team we should work
+//with the common resources, when available.
+int main() {
+
+}
