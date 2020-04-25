549841168b87850af791342df9f18add19fc53d8
nguyejo4 <59853790+nguyejo4@users.noreply.github.com>
2020-04-24 21:01:17 -0700

    Moving Test Piece
    Implemented a test piece and the ability to move it using the right arrow on the keyboard.

--- sfml_test/sfml_test/main.cpp
+++ sfml_test/sfml_test/main.cpp
@@ -11,6 +11,7 @@ void writeToLog(string message, string variable);
 
 int main()
 {
+	//making a matrix representing the board
 	boardTile** boardArray = new boardTile * [27];
 	for (int i = 0; i < 27; i++) {
 		boardArray[i] = new boardTile[27];
@@ -19,6 +20,7 @@ int main()
 	boardTile floor = boardTile("f");
 	boardTile wall = boardTile("w");
 
+	//opening csv file to fill board data
 	ifstream dataSource;
 	dataSource.open("ClueBoardCSV.csv");
 	if (dataSource.fail()) {
@@ -35,6 +37,8 @@ int main()
 	string line, word, temp;
 	int row = 0;
 	int col = 0;
+	
+	//filling the board based on data from the CSV
 	while (!dataSource.eof()) {
 		string tempStringArray[27];
 		col = 0;
@@ -53,30 +57,35 @@ int main()
 	cout << "Board of chars: " << endl;
 	gameBoard.charBoard();
 
-	sf::RenderWindow window(sf::VideoMode(800, 800), "SFML works!");
+	//graphical output section
+	//creating a render window with SFML
+	sf::RenderWindow window(sf::VideoMode(1280,800), "Clue!", sf::Style::Default);
 	
 	//making textures
 	sf::Texture room_texture;
 	if (!room_texture.loadFromFile("room.bmp"))
 	{
-		// error...
+		cout << "Cannot open room.bmp" << endl;
 	}
 
 	sf::Texture wall_texture;
 	if (!wall_texture.loadFromFile("wall.bmp"))
 	{
-		// error...
+		cout << "Cannot open wall.bmp" << endl;
 	}
 
 	sf::Texture floor_texture;
 	if (!floor_texture.loadFromFile("floor.bmp"))
 	{
-		// error...
+		cout << "Cannot open floor.bmp" << endl;
 	}
+
+	//making mustard colored test piece
+	sf::CircleShape triangle(30, 3);
+	triangle.setFillColor(sf::Color(225, 173, 1));
 	
 	//making a matrix of empty sprites
 	sf::Sprite** rendered_board = new sf::Sprite * [27];
-
 	for (int i = 0; i < 27; i++)
 	{
 		rendered_board[i] = new sf::Sprite[27];
@@ -88,13 +97,16 @@ int main()
 		for (int j = 0; j < 27; j++)
 		{
 			//setting coordinates in window
-			double x_position = 24.0 * i;
-			double y_position = 24.0 * j;
+			double x_position = 14.0 * i;
+			double y_position = 14.0 * j;
 
+			//setting the origin of each sprite
 			rendered_board[i][j].setOrigin(sf::Vector2f(x_position, y_position));
 
 			char currentRoom = boardArray[i][j].charTile();
 
+			//setting textures according to the tile's type
+			//room
 			if (currentRoom == 'R')
 			{
 				rendered_board[i][j].setTexture(room_texture);
@@ -114,10 +126,12 @@ int main()
 		}
 	}
 
+	cout << "Press the right arrow key to move the piece." << endl;
 
+	//rendering window
     while (window.isOpen())
     {
-        
+		//sf::Time move_delay = sf::seconds(0.1f);
         sf::Event event;
         while (window.pollEvent(event))
         {
@@ -131,6 +145,7 @@ int main()
 		//sprite.setTexture(wall_texture);
 		//window.draw(sprite);
 		
+		//drawing board's sprites
 		for (int i = 0; i < 27; i++)
 		{
 			for (int j = 0; j < 27; j++)
@@ -139,7 +154,36 @@ int main()
 			}
 		}
 
+		//putting test piece on board
+		//order matters on SFML
+		//last sprite drawn is on top
+		window.draw(triangle);
         window.display();
+
+		//commented out this block of code
+		//it was testing time based movement delays
+		/*sf::Time elapsed = clock.getElapsedTime();
+		//testing moving the piece
+		if ((pieceMoved == false) && (elapsed > sf::seconds(1.0f)))
+		{
+			window.clear();
+			triangle.move(sf::Vector2f(100.0, 100.0));
+			window.display();
+			pieceMoved = true;
+		}*/
+
+		//test code for moving a piece
+		//right arrow will move the piece to the bottom right
+		if (event.type == sf::Event::KeyPressed)
+		{
+			if (event.key.code == sf::Keyboard::Right)
+			{
+				window.clear();
+				triangle.move(sf::Vector2f(25.0, 25.0));
+				window.display();
+			}
+		}
+
     }
 
     return 0;
