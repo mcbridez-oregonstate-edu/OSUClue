da00c5e9c38c66fe1794ce4b31db28cdc8669905
this-is-my-serious-account <56110943+this-is-my-serious-account@users.noreply.github.com>
2020-05-18 03:12:23 -0400

    Reverting to original layout of server/client stuff because I like it better

---
+++ Server.hpp
@@ -0,0 +1,30 @@
+/********************************************************************************
+ * Program Name: Server.hpp
+ * Author: Abigail Minchella
+ * Date: 5/14/20
+ * Description: The header file for Server
+********************************************************************************/
+#ifndef SERVER_HPP
+#define SERVER_HPP
+
+#include <SFML/Network.hpp>
+#include <string>
+using std::string;
+
+class Server {
+    protected:
+        int port;
+        sf::TcpListener listener;
+        sf::TcpSocket clients[6];        
+        sf::SocketSelector selector;
+        int numClients;
+
+    public:
+        Server(int);
+        void acceptClient();
+        void sendOne(sf::Packet, int);
+        void sendAll(sf::Packet);
+        sf::Packet receiveData();
+};
+
+#endif
