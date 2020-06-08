87f81ceb0a850d83dd302829b03c886e76b0138a
this-is-my-serious-account <56110943+this-is-my-serious-account@users.noreply.github.com>
2020-06-07 22:09:53 -0400

    Update Client.cpp

--- Client.cpp
+++ Client.cpp
@@ -19,7 +19,7 @@ Client::Client(sf::IpAddress server, int serverPort)
     serverIP = server;
     port = serverPort;
 
-    sf::Socket::Status status = socket.connect(serverIP, port, sf::microseconds(5.f));
+    sf::Socket::Status status = socket.connect(serverIP, port, sf::seconds(5.f));
     if (status != sf::Socket::Done)
     {
         success = false;
